96e2f78498bde25c05e6969e1e8f528959b12586
Avinash Kumar <avinashbest3@gmail.com>
2021-05-19 13:21:08 +0530

    Added: Insert, Find & Delete Queries in Android Room Database

--- app/src/main/java/com/example/notestaking/NoteDao.kt
+++ app/src/main/java/com/example/notestaking/NoteDao.kt
@@ -1,5 +1,6 @@
 package com.example.notestaking
 
+import androidx.lifecycle.LiveData
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -7,11 +8,11 @@ import androidx.room.Query
 
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Delete
-    fun delete(note: Note)
+    suspend fun delete(note: Note)
 
     @Query("Select * from notes_table order by id ASC")
-    fun getAllNotes(): List<Note>
+    fun getAllNotes(): LiveData<List<Note>>
 }
